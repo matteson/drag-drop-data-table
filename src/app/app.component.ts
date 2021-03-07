95d11a81d374a0cf322e5d1552432098478626c1
Andrew <drew.matteson@gmail.com>
2021-03-06 20:49:49 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+export interface PeriodicElement {
+  name: string;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {name: 'Boron', weight: 10.811, symbol: 'B'},
+  {name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +27,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sortable-data-table';
+  @ViewChild('dataTable') table: MatTable<PeriodicElement>;
+  displayedColumns: string[] = ['name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  dropTable(event: CdkDragDrop<PeriodicElement[]>): void {
+    moveItemInArray(this.dataSource, event.previousIndex, event.currentIndex);
+    this.table.renderRows();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    MatTableModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
